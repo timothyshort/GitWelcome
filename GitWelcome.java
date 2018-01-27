24bf2a76dd2a0d44be7c9f2ca6484cfa0bab4c1c
Tim Short <timothyjames.short@gmail.com>
2018-01-26 22:43:35 -0500

    Initialized source code

--- GitWelcome.java
+++ GitWelcome.java
@@ -1,8 +1,8 @@
 public class GitWelcome {
   public static void main(String[] args) {
-    System.out.println("Freddie Training");
+    System.out.println("Welcome to _____ Training");
     System.out.println("Cohort: ");
-    System.out.println("Today's date is: ")
-    System.out.println("My name is: ")
+    System.out.println("Today's date is: ");
+    System.out.println("My name is: ");
   }
 }
